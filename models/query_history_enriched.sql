f36dc2806c44abe4fd0f71faf7ac89fee7117d10
Konstantin Baidin <128369285+konstantin-baidin-y42@users.noreply.github.com>
2024-01-26 20:20:24 +0400

    Update query_history_enriched.sql

--- models/query_history_enriched.sql
+++ models/query_history_enriched.sql
@@ -17,8 +17,7 @@ query_history as (
             when try_parse_json(query_tag)['dbt_snowflake_query_tags_version'] is not null then try_parse_json(query_tag)
         end as _dbt_json_query_tag_meta,
         case
-            when _dbt_json_comment_meta is not null or _dbt_json_query_tag_meta is not null then
-                {{ adapter.quote_as_configured(this.database, 'database') }}.{{ adapter.quote_as_configured(this.schema, 'schema') }}.merge_objects(coalesce(_dbt_json_comment_meta, { }), coalesce(_dbt_json_query_tag_meta, { }))
+            when _dbt_json_comment_meta is not null or _dbt_json_query_tag_meta is not null then merge_objects(coalesce(_dbt_json_comment_meta, { }), coalesce(_dbt_json_query_tag_meta, { }))
         end as dbt_metadata
 
     from {{ ref('stg_query_history') }}
